f952df2f507548c0813cb86fcdc328df6b1dd911
ghashy <obsidian.musicwork@gmail.com>
2023-03-17 21:49:40 +0300

    fix the generator

--- Resources/NeckGenerator/Generator.gd
+++ Resources/NeckGenerator/Generator.gd
@@ -7,7 +7,8 @@ var d: int
 var result: Dictionary
 var pairs: Array[String] = []
 var idx: int = 0
-var last_s = 0;
+var last_s = 0
+var last_d = 0
 
 func _init() -> void:
 	for i in range(0, 25):
@@ -16,30 +17,31 @@ func _init() -> void:
 func get_values() -> Dictionary:
 
 	# First step generation.
-	s = get_s_value();
-	d = get_d_value();
+	s = get_s_value()
+	d = get_d_value()
 
 	# Fix duplicated pairs.
-	fix();
+	fix()
 
 	# Add to vector.
-	pairs[idx] = str(s) + "-" + str(d);
+	pairs[idx] = str(s) + "-" + str(d)
 
 	last_s = s
+	last_d = d
 	if idx < 24:
 		idx += 1
 	else:
-		idx = 0;
+		idx = 0
 
 	result["string"] = s
 	result["degree"] = d
-	return result;
+	return result
 
 func fix() -> void:
-	var key: String = str(s) + "-" + str(d);
-	while pairs.find(key) != -1 or s == last_s:
-		s = get_s_value();
-		d = get_d_value();
+	var key: String = str(s) + "-" + str(d)
+	while pairs.find(key) != -1 or s == last_s or d == last_d:
+		s = get_s_value()
+		d = get_d_value()
 		key = str(s) + "-" + str(d)
 
 func is_duplicated_two_times() -> bool:
@@ -48,14 +50,14 @@ func is_duplicated_two_times() -> bool:
 			var t_d = pairs[i][0].to_int()
 			var t_s = pairs[i][2].to_int()
 			if t_d == d and t_s == s: return true
-	return false;
+	return false
 
 func get_d_value() -> int:
 	var rand: RandomNumberGenerator = RandomNumberGenerator.new()
-	rand.randomize();
-	return rand.randi_range(1, 7);
+	rand.randomize()
+	return rand.randi_range(1, 7)
 
 func get_s_value() -> int:
 	var rand: RandomNumberGenerator = RandomNumberGenerator.new()
-	rand.randomize();
-	return rand.randi_range(2, 6);
+	rand.randomize()
+	return rand.randi_range(2, 6)
